8d605985754ca8ecc8d89aa5ebd5efa3d97c9846
Brendan Labadie <brendanlabadie@u.northwestern.edu>
2017-12-18 20:37:20 -0500

    Modified templates to display posts from database.

---
+++ tickets/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.ticket_list, name='ticket_list'),
+]
--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Ticket
+
+def ticket_list(request):
+	tickets = Ticket.objects.filter(date_seen__lte=timezone.now()).order_by('date_seen')
+	return render(request, 'tickets/ticket_list.html', {'tickets': tickets})
 
 # Create your views here.
