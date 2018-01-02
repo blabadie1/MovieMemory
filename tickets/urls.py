7efb80e98852024da1bb2532ce67545b08af3740
Brendan Labadie <brendanlabadie@u.northwestern.edu>
2018-01-01 23:11:26 -0500

    Completed CSS for ticket display page and created basic form to create new ticket.

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ticket_list, name='ticket_list'),
+	url(r'^ticket/new/$', views.ticket_new, name='ticket_new'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Ticket
+from .forms import TicketForm
+from django.shortcuts import redirect
 
 def ticket_list(request):
 	tickets = Ticket.objects.filter(date_seen__lte=timezone.now()).order_by('date_seen')
 	return render(request, 'ticket_list.html', {'tickets': tickets})
 
+def ticket_new(request):
+	if request.method == "POST":
+		form = TicketForm(request.POST)
+		if form.is_valid():
+			ticket = form.save()
+			tickets = Ticket.objects.filter(date_seen__lte=timezone.now()).order_by('date_seen')
+			return render(request, 'ticket_list.html', {'tickets': tickets})
+			#return render(request, 'ticket_list.html')
+	else:
+		form = TicketForm()
+	return render(request, 'ticket_edit.html', {'form': form})
 # Create your views here.
