e230bf9a1824533c14792075de741633d9dbe14e
Brendan Labadie <brendanlabadie@u.northwestern.edu>
2018-01-22 21:12:24 -0600

    Mobile support, model changes

--- tickets/forms.py
+++ tickets/forms.py
@@ -7,13 +7,11 @@ class TicketForm(forms.ModelForm):
 		model = Ticket
 		fields = ('title', 'date_seen', 'companions', 'genre', 'location', 'notes')
 		widgets = {
-			'date_seen': forms.TextInput(attrs={'placeholder': 'Use format "D/M/Y 19:30"'}),
+			'date_seen': forms.TextInput(attrs={'placeholder': 'Use format "12/30/2017 19:30"'}),
 			'notes': forms.TextInput(attrs={'placeholder': 'Enter what you learned, or what you liked'}),
 			'title': forms.TextInput(attrs={'autocomplete': 'off'}),
-			#'date_seen': forms.TextInput(attrs={'autocomplete': 'off'}),
 			'companions': forms.TextInput(attrs={'autocomplete': 'off'}),
 			'genre': forms.TextInput(attrs={'autocomplete': 'off'}),
 			'location': forms.TextInput(attrs={'autocomplete': 'off'}),
-			#'notes': forms.TextInput(attrs={'autocomplete': 'off'}),
 		}
 		labels = {'companions': ('Fellow viewers'), 'notes': ('My thoughts'),}
